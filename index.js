74f7384ba77893b842b7501892cacb448e9bf434
Nate <npanajotov@2create.bg>
2022-09-27 18:01:34 +0300

    Fix port

--- index.js
+++ index.js
@@ -18,9 +18,7 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+server.listen(process.env.PORT)
 
 
 async function scrape() {
