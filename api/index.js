57ebdc3577e32f94ad1efef13eb2fa6c27b9da1b
Nate <npanajotov@2create.bg>
2022-09-28 14:36:13 +0300

    Add more tests

--- api/index.js
+++ api/index.js
@@ -8,9 +8,46 @@ app.get('/api', (req, res) => {
   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
+app.get('/api/results', (req, res) => {
+  async function scrape() {
+		const browser = await puppeteer.launch({})
+		const page = await browser.newPage();
+		const scrapeData = {};
+
+		await page.goto('https://mubz.bg/#!boss#all', {
+			waitUntil: 'load'
+		});
+
+		setTimeout(() => {
+			const content = page.content();
+
+			content.then(data => {
+				const bosses = data.split('Last Kill by:').pop().split('Refresh')[0].split('<tbody>').pop().split('</tbody>')[0];
+
+				scrapeData.bosses = bosses;
+
+				page.goto('https://mubz.bg/#!golden_mobs#all', {
+					waitUntil: 'load'
+				}).then(data => {
+					setTimeout(() => {
+						const goldenContnt = page.content();
+
+						goldenContnt.then(data => {
+							const golden = data.split('Golden Monster:').pop().split('Refresh')[0].split('<tbody>').pop().split('</tbody>')[0];
+
+							scrapeData.golden = golden;
+
+							res.json(scrapeData)
+
+							browser.close();
+						})
+					}, 2000);
+				})
+			})
+		}, 2000);
+	}
+
+	scrape();
 });
 
 module.exports = app;
--- index.js
+++ index.js
@@ -12,44 +12,4 @@ server.listen(3000, () => {
   console.log('listening on *:3000');
 });
 
-app.get('/api/results', (req, res) => {
-	async function scrape() {
-		const browser = await puppeteer.launch({})
-		const page = await browser.newPage();
-		const scrapeData = {};
 
-		await page.goto('https://mubz.bg/#!boss#all', {
-			waitUntil: 'load'
-		});
-
-		setTimeout(() => {
-			const content = page.content();
-
-			content.then(data => {
-				const bosses = data.split('Last Kill by:').pop().split('Refresh')[0].split('<tbody>').pop().split('</tbody>')[0];
-
-				scrapeData.bosses = bosses;
-
-				page.goto('https://mubz.bg/#!golden_mobs#all', {
-					waitUntil: 'load'
-				}).then(data => {
-					setTimeout(() => {
-						const goldenContnt = page.content();
-
-						goldenContnt.then(data => {
-							const golden = data.split('Golden Monster:').pop().split('Refresh')[0].split('<tbody>').pop().split('</tbody>')[0];
-
-							scrapeData.golden = golden;
-
-							res.json(scrapeData)
-
-							browser.close();
-						})
-					}, 2000);
-				})
-			})
-		}, 2000);
-	}
-
-	scrape();
-});
